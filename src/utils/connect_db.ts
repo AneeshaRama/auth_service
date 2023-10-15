d3b4d6491fe57958c7a4fff3a14563f0f344fd92
AneeshaRama <aneesharama@gmail.com>
2023-10-15 17:41:48 +0530

    Created express server

--- src/utils/connect_db.ts
+++ src/utils/connect_db.ts
@@ -1,1 +1,9 @@
-// logic to connect mongodb
+import {connect} from "mongoose";
+
+
+export const connectToDatabase = async ()=>{
+    await connect(process.env.MONGODB_URI!)
+        .then(()=>{
+            console.log(`Connected to DB`);
+        });
+}
